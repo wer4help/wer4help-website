5f41aeef646e9f9ab0aceac605f55d587852e7e0
PATHIVADA KEERTHANA <147787878+keerthanapathivada@users.noreply.github.com>
2026-03-12 02:04:34 +0530

    Refactor routes and add new leadership and impact pages

--- app.py
+++ app.py
@@ -6,37 +6,47 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
 
-@app.route("/events")
-def events():
-    return render_template("events.html")
+@app.route("/leadership")
+def leadership():
+    return render_template("leadership.html")
+
 
 @app.route("/team")
 def team():
     return render_template("team.html")
 
-@app.route("/activity-reports")
-def activity_reports():
-    return render_template("leadership.html")
 
-@app.route("/orientation")
-def orientation():
-    return render_template("orientation.html")
+@app.route("/events")
+def events():
+    return render_template("events.html")
 
-@app.route("/vighna-vinuta")
-def vighna_vinuta():
-    return render_template("vighna_vinuta.html")
+
+@app.route("/impact")
+def impact():
+    return render_template("impact.html")  
+# change later if you create impact.html
+
+
+@app.route("/acknowledgment")
+def acknowledgment():
+    return render_template("acknowledgment.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 
 @app.route("/donate")
 def donate():
     return render_template("donate.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
